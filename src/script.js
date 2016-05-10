f43a31666fc031ea949d187f3126696a8a55ef97
renato santos <renatojs.web@gmail.com>
2016-05-10 05:01:24 +0000

    first

---
+++ src/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	
+
+	// itens
+	var rodaAtual = 1;
+	var carAtual = 1;
+
+	// muda carro
+	$('#display-car').css('background-image', 'url("img/carro/car1.jpg")');
+
+	// muda roda
+	$('#display-roda').css('background-image', 'url("img/roda/roda1.png")');
+
+});
+
+
+// 
+function  mudaRoda(rodaAtual) {
+	$('#display-roda').css('background-image', 'url("img/roda/roda'+rodaAtual+'.png")');	
+	return false;
+}
+
+// 
+function mudaCarro(carroAtual) {
+	$('#display-car').css('background-image', 'url("img/carro/car'+carroAtual+'.jpg")');	
+	return false;
+}
+
